2ee94b13d124986455da88aeb6ed9c1828cc137e
Thomas Beverley <Thomas101@users.noreply.github.com>
2024-10-18 14:09:27 +0100

    Remove a reference to window in the service worker

--- src/shared/Hash.ts
+++ src/shared/Hash.ts
@@ -5,7 +5,7 @@
  */
 export async function sha256 (str: string) {
   const msgBuffer = new TextEncoder().encode(str)
-  const hashBuffer = await window.crypto.subtle.digest('SHA-256', msgBuffer)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
   const hashArray = Array.from(new Uint8Array(hashBuffer))
   const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
   return hashHex
