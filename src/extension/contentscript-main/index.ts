ae2075cf86aa60ef8cf18c71c5974d8551225b22
Thomas Beverley <Thomas101@users.noreply.github.com>
2025-01-08 13:51:58 +0000

    Possibly fix the @aibrow/dom-types import in the web library

--- src/extension/contentscript-main/index.ts
+++ src/extension/contentscript-main/index.ts
@@ -1,5 +1,30 @@
 import AI from './AI'
 import Translation from './Translation'
+import {
+  AICapabilities,
+  AICapabilityAvailability,
+  AICapabilityGpuEngine,
+  AIHelperInstalledState
+} from '#Shared/API/AI'
+import {
+  AILanguageModelInitialPromptRole,
+  AILanguageModelPromptRole
+} from '#Shared/API/AILanguageModel/AILanguageModelTypes'
+import {
+  AIRewriterFormat,
+  AIRewriterLength,
+  AIRewriterTone
+} from '#Shared/API/AIRewriter/AIRewriterTypes'
+import {
+  AISummarizerFormat,
+  AISummarizerLength,
+  AISummarizerType
+} from '#Shared/API/AISummarizer/AISummarizerTypes'
+import {
+  AIWriterFormat,
+  AIWriterLength,
+  AIWriterTone
+} from '#Shared/API/AIWriter/AIWriterTypes'
 
 export const ai = new AI(window.ai)
 export const translation = new Translation(ai, (window as any).translation)
@@ -15,3 +40,24 @@ genericWindow.aibrow = ai
 genericWindow.aibrowTranslation = translation
 
 export default ai
+export {
+  AICapabilities,
+  AICapabilityAvailability,
+  AICapabilityGpuEngine,
+  AIHelperInstalledState,
+
+  AILanguageModelInitialPromptRole,
+  AILanguageModelPromptRole,
+
+  AIRewriterFormat,
+  AIRewriterLength,
+  AIRewriterTone,
+
+  AISummarizerFormat,
+  AISummarizerLength,
+  AISummarizerType,
+
+  AIWriterFormat,
+  AIWriterLength,
+  AIWriterTone
+}
--- src/web-library/library/AibrowAI.ts
+++ src/web-library/library/AibrowAI.ts
@@ -1,10 +1,9 @@
-//TODO fix
-/*import type AIBrow from '@aibrow/dom-types'
+import type AIBrow from '@aibrow/dom-types'
 
 declare global {
   interface Window {
     readonly aibrow: typeof AIBrow;
   }
-}*/
+}
 
-export default (window as any).aibrow as any
+export default window.aibrow
