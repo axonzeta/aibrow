49ef9601c0f7d487b0fc368ea1318430247358a5
Thomas Beverley <Thomas101@users.noreply.github.com>
2025-04-02 15:12:10 +0100

    Dont override the M136 APIs until the compatibility layer is complete

--- src/extension/contentscript-main/index.ts
+++ src/extension/contentscript-main/index.ts
@@ -27,39 +27,22 @@ import {
   AIWriterTone
 } from '#Shared/API/AIWriter/AIWriterTypes'
 
-export const ai = new AI(globalThis.ai)
-export const translation = new Translation(ai, (globalThis as any).translation)
+export const ai = new AI(window.ai)
+export const translation = new Translation(ai, (window as any).translation)
 
-const aGlobalThis = globalThis as any
+const genericWindow = window as any
 
 // Add overrides for the M135- origin trials
-if (!globalThis.ai || aGlobalThis.ai?.__aibrowOverride === true) {
-  aGlobalThis.ai = ai
+if (!genericWindow.ai || genericWindow.ai?.__aibrowOverride === true) {
+  genericWindow.ai = ai
 }
-if (!aGlobalThis.translation || aGlobalThis.translation?.__aibrowOverride === true) {
-  aGlobalThis.translation = translation
-}
-
-// Add overrides for the M136+ origin trials
-if (!aGlobalThis.LanguageModel) {
-  aGlobalThis.LanguageModel = ai.languageModel
-}
-if (!aGlobalThis.Summarizer) {
-  aGlobalThis.Summarizer = ai.summarizer
-}
-if (!aGlobalThis.Writer) {
-  aGlobalThis.Writer = ai.writer
-}
-if (!aGlobalThis.Rewriter) {
-  aGlobalThis.Rewriter = ai.rewriter
-}
-if (!aGlobalThis.Translator) {
-  aGlobalThis.Translator = translation
+if (!genericWindow.translation || genericWindow.translation?.__aibrowOverride === true) {
+  genericWindow.translation = translation
 }
 
 // Expose aibrow
-aGlobalThis.aibrow = ai
-aGlobalThis.aibrowTranslation = translation
+genericWindow.aibrow = ai
+genericWindow.aibrowTranslation = translation
 
 export default ai
 export {
