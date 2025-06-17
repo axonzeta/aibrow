e95e3820eca5d00ebc99bdb104a64baa3a1020d4
Thomas Beverley <Thomas101@users.noreply.github.com>
2025-06-17 14:41:30 +0100

    A really terrible hack for path resolution for rollup on win32

--- src/extension-library/types-rollup.config.js
+++ src/extension-library/types-rollup.config.js
@@ -8,7 +8,11 @@ export default [{
       tsConfigPath: path.join(import.meta.dirname, 'tsconfig.json'),
       absolute: false,
       preserveExtensions: true,
-      transform: (p) => path.resolve('/', p)
+      transform: (p) => {
+        return process.platform === 'win32' && p.startsWith('.\\C:\\')
+          ? p.slice(2)
+          : path.resolve('/', p)
+      }
     }),
     dts()
   ]
